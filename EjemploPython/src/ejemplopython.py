8c30060bdf85d841588c8b38cf9beef60680fde7
roger <roger@192.168.1.9>
2021-01-03 14:02:13 -0500

    Primer Cambio

---
+++ EjemploPython/src/ejemplopython.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "roger"
+__date__ = "$03-ene-2021 13:58:49$"
+
+print("Hola")
+ 
